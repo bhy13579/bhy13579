811b3ebacb5ce7b0d5bf0cb887403877a93c6737
hoyeong <bhy13579@gmail.com>
2026-09-15 17:48:43 +0900

    [Passed] 5의 배수의 합 (46ms, 16MB)

---
+++ trail1/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+count = 0
+
+if a <= b:
+    for i in range(a, b+1):
+        if i % 5 == 0:
+            count += i
+else:
+    for i in range(b, a + 1):
+        if i % 5 == 0:
+            count += i
+
+print(count)
